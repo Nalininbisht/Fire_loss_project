3b2417aaa7cd8895e64069c4923eee5c68a2e2cb
Nalini Bisht <nalinibisht56@gmail.com>
2025-02-09 22:14:28 +0530

    Add files via upload

---
+++ Machine Learning/app.py
@@ -0,0 +1,27 @@
+
+import pickle
+import pandas as pd
+import sklearn
+from flask import Flask, render_template, request
+
+model = pickle.load(open('trained_model_7.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')  
+    else:
+            form_inputs = pd.DataFrame(request.form.to_dict(), index=[0])
+
+            prediction = model.predict(form_inputs.astype(float))
+
+            return str(prediction)
+
+        
+           
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
